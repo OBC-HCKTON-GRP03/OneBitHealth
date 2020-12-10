21b0465af905e9b49f154a096a4cb76493b2b074
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-12-10 19:58:58 -0300

    Adding partial to serve the dashboard and javascript for dynamism of the sidebar.

---
+++ app/javascript/menu_dashboard.js
@@ -0,0 +1,13 @@
+(function($) {
+	var fullHeight = function() {
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+})(jQuery);
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
     resources :exams
     resources :treatments
   end
+  get '/dashboard', to: 'dashboard#index'
   root 'home#index'
-
+  
   devise_for :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
