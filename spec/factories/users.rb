3989319f893ec57fb903163947ee0be84093cd43
Senott <senott@yahoo.com>
2020-12-11 12:05:57 -0300

    Authentication accpetance tests.

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 8) }
+  end
+end
