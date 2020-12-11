0a4f6bec45c7014e0aabf846640199c2d636e57f
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-12-11 11:05:31 -0300

    Included button to enter and logout the user's dashboard in the home.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,10 @@
 class DashboardController < ApplicationController
+	before_action :authenticate_user! 
 	layout "dashboard"
+	
 	def index
-		@appointments = Appointment.all
-		#@exams = @appointment.exams
-		#@treatments = @appointment.treatments
+		@appointments = Appointment.order(updated_at: :desc).where(user_id: current_user.id).take(3)
+		@exams = Exam.order(updated_at: :desc).where(appointment_id: @appointments).take(3)
+		@treatments = Treatment.order(updated_at: :desc).where(appointment_id: @appointments).take(3)
 	end
 end
