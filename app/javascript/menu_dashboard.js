92460c2f9cf60ee0aabc4b9377e384888133c685
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-12-12 11:45:40 -0300

    Adjusting the partial header with a button to open and close the new dashboard in addition to a main button and another one to exit, on the side.

--- app/javascript/menu_dashboard.js
+++ app/javascript/menu_dashboard.js
@@ -11,32 +11,45 @@
 
 	$('#sidebarCollapse').on('click', function () {
     $('#sidebar').toggleClass('active');
-    
+
+
     if(contraid){
-    	$('.img-logo-auth').attr("src","/assets/avatar_teste.svg").css({"height":"80%", "width":"80%"}); // implement other image
-    	contraid = false;
+        $('.img-logo-auth').attr("src","/assets/avatar_teste.svg").css({"height":"80%", "width":"80%"}); // implement other image
     }else{
-    	$('.img-logo-auth').attr("src","/assets/logotipo_onebit_health_black.svg");
-    	contraid = true;
+        $('.img-logo-auth').attr("src","/assets/logotipo_onebit_health_black.svg");
     }
 
     if(contraid){
-    	$("a.my_history span").remove();
-    }else{
     	$("a.my_history img").after("<span class='text_inner_links'>  Meu Histórico </span>");
+    }else{
+        $("a.my_history span").remove();
     }
 
     if(contraid){
-    	$("a.histories_shares span").remove();
+        $("a.histories_shares img").after("<span class='text_inner_links'> Históricos <p>Compartilhados</p> </span>");    	
     }else{
-    	$("a.histories_shares img").after("<span class='text_inner_links'> Históricos <p>Compartilhados</p> </span>");
+        $("a.histories_shares span").remove();
     }
 
     if(contraid){
-    	$("button.share_history span").remove();
+        $("button.share_history img").after("<span class='text_inner_links'>  Compartilhar </span>");    	
     }else{
-    	$("button.share_history img").after("<span class='text_inner_links'>  Compartilhar </span>");
+        $("button.share_history span").remove();
     }
+
+    if(contraid){
+        $("#arrow_side").attr("src", "/assets/icon_arrow_left.svg");
+    }else{
+        $("#arrow_side").attr("src", "/assets/icon_arrow_right.svg");
+    }
+
+    if(contraid){
+       contraid = false; 
+   }else{
+       contraid = true;
+   }
+
   });
+
 })(jQuery);
 
