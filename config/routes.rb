738fc715c350f8a3984b2385b9fd245d1d160554
rickutino <rickutino@gmail.com>
2020-12-12 13:04:10 +0900

    correção do route

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,16 @@ Rails.application.routes.draw do
   end
   
   get 'home', to: 'home#index'
+  
+  get '/dashboard', to: 'dashboard#index'
+  
+  devise_for :users
+  get '/user' => 'dashboard#index', :as => :user_root
+
+  ## Essas linha de codigo tem que esta por ultimo.
   get 'fatal_test', to: 'errors#fatal_test'
   get 'error_test', to: 'errors#error_test'
 
   match '*path', to: 'errors#not_found', via: :all
-
-  get '/dashboard', to: 'dashboard#index'
-
-  devise_for :users
-  get '/user' => 'dashboard#index', :as => :user_root
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  ##
 end
