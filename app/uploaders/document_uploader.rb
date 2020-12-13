87f9ad8d82199be097faec2b68d5c1f8d86c5cf3
Haroldo Furtado <haroldofurtado@gmail.com>
2020-12-13 02:07:49 -0300

    Add extension white list

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -35,9 +35,9 @@ class DocumentUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png pdf)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
