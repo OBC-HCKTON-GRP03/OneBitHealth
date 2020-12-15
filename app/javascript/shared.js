923e1b83cdfe5a8757c2d786f1ba1d1c3be4bda5
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-12-15 02:08:10 -0300

    Implementing the modal, creating the search by email and developing jquery that will keep you SPA.

--- app/javascript/shared.js
+++ app/javascript/shared.js
@@ -4,6 +4,8 @@ $(document).ready(() => {
 	let val = '';
 
 	timer = 0
+	// user search functionality via ajax
+
 	$('#shared_search').keyup(() => {
 		clearTimeout(timer);
 		val = $('#shared_search').val();
@@ -23,8 +25,8 @@ $(document).ready(() => {
 			  	response["shared_users"].forEach((item, index) => {
 		  			tbody.append(`
 		  				<tr>
-		  					<td>${item.email}</td>
-		  					<td><a rel="nofollow" data-method="post" href="/shared_users?i_share_with_id=${item.id}&amp;user_id=${current_user_id}">Adicionar</a></td>
+		  					<td class="text-center add_share"><a key=${item.id} rel="nofollow" data-method="post" href="/shared_users?i_share_with_id=${item.id}&amp;user_id=${current_user_id}"><img src="/assets/icon_user_add.svg"></a></td>
+		  					<td class="text-center">${item.name}</td>
 		  				</tr>
 		  			`)
 		  		})
@@ -34,4 +36,16 @@ $(document).ready(() => {
 			$(".modal-body table.not-shared-yet tbody").html("")
 		}
 	})
-})	
+
+	$("td.add_share a").on(() => {
+			let item_id = event.target.key
+			let current_user_id = current_user.id
+
+			$.ajax({
+				url: `/shared_users?i_share_with_id=${item.id}&amp;user_id=${current_user_id}`,
+			  type: 'POST',
+			  success: function (response) {
+			  	var tbody = $(".modal-body table.not-shared-yet tbody")
+			  	var current_user_id = response['current_user_id']
+	})
+})
