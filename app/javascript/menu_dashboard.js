cc0ae0cd5dc5a096287202b715d07b75e4c65c88
Senott <senott@yahoo.com>
2020-12-17 10:18:58 -0300

    Adjusting some icons that could not be shown when using javascript.

--- app/javascript/menu_dashboard.js
+++ app/javascript/menu_dashboard.js
@@ -16,13 +16,13 @@
 
 
     if(contraid){
-        $('.img-logo-auth').attr("src","/assets/avatar_teste.svg").css({"height":"80%", "width":"80%"}); // implement other image
+        $('.img-logo-auth').attr("src","avatar_teste.svg").css({"height":"80%", "width":"80%"}); // implement other image
         $("a#my_history img").after("<span class='text_inner_links'>  Meu Histórico </span>");
         $("a#histories_shares img").after("<span class='text_inner_links'> Históricos <p>Compartilhados</p> </span>");      
         $("button#share_history img").after("<span class='text_inner_links'>  Compartilhar </span>");       
         $("#arrow_side").attr("src", "/assets/icon_arrow_left.svg");
     }else{
-        $('.img-logo-auth').attr("src","/assets/logotipo_onebit_health_black.svg");
+        $('.img-logo-auth').attr("src","logotipo_onebit_health_black.svg");
         $("a#my_history span").remove();
         $("a#histories_shares span").remove();
         $("button#share_history span").remove();
